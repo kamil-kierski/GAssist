4b0a54796c359db4c976392aa8554e08b2619248
Kamil Kierski <cybernetic87@gmail.com>
2019-07-21 15:02:18 +0200

    missing files

---
+++ GAssist/PowerLock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace GAssist
+{
+    class PowerLock
+    {
+        internal static void RequestScreenLock(int timeout)
+        {
+            Assembly assembly = AppDomain.CurrentDomain.GetAssemblies().First(a => a.GetName().Name == "Tizen.System");
+
+            Type type = assembly.GetTypes().First(t => t.Name == "Device" && t.IsClass);
+
+            MethodInfo methodInfo = type.GetRuntimeMethods().First(m => m.Name == "DevicePowerRequestLock");
+
+            methodInfo.Invoke(null, new object[] { 1, timeout });
+        }
+
+        //static void ReleaseLock()
+        //{
+        //TODO
+        //}
+
+    }
+}
